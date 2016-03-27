d5bc6bce1cdcd96052e68da82b615ba61fcce37e
pihh <pihh.rocks@gmail.com>
2016-03-27 00:38:20 +0000

    small improvements

--- database/database.php
+++ database/database.php
@@ -58,27 +58,25 @@ class mDatabase implements iConnection, iDatabase{
         // If connection not set
         if(!static::$connection) {
 
-           $config = Config::get('database');
-           $driver = $config['driver'];
-           $charset = $config['charset'];
 
            // Fetch parameters
 
-           if ($driver == 'mysql') {
+           if ($_ENV['DB_DRIVER'] == 'mysql') {
                 try {
-                    static::$connection = new \PDO('mysql:host=' . $config['host'] . ';dbname=' . $config['database'], $config['username'], $config['password']);
+                    static::$connection = new \PDO('mysql:host=' . $_ENV['DB_HOST'] . ';dbname=' . $_ENV['DB_DATABASE'], $_ENV['DB_USERNAME'], $_ENV['DB_PASSWORD']);
                     static::$connection->setAttribute(\PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                     static::$connection_status = self::$connection->getAttribute(PDO::ATTR_CONNECTION_STATUS);
-                    static::$connection->query("SET CHARACTER SET $charset");
+                    static::$connection->query('SET CHARACTER SET '.$_ENV['DB_CHARSET']);
                 } catch (PDOException $e) {
                     echo $e->getMessage();
                     exit;
                 }
             }
 
-            if ($driver === 'SQLite3') {
+            if ($_ENV['DB_DRIVER'] === 'SQLite3') {
                 try {
-                    static::$connection = new \PDO('sqlite:'.ROOT.DS.'app'.DS.'files'.DS.'database'.DS.'databases'.DS.$config['database'].'.sq3');
+                    $sq3Path = ROOT.DS.'app'.DS.'files'.DS.'database'.DS.'databases'.DS.$_ENV['DB_DATABASE'].'.sq3';
+                    static::$connection = new \PDO('sqlite:'.$sq3Path);
                     static::$connection_status = self::$connection->getAttribute(PDO::ATTR_CONNECTION_STATUS);
                 } catch (PDOException $e) {
                     echo $e->getMessage();
